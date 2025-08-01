af3183ba062efffd6ee9e76e99ccd90ac540a562
KingNullboy <mjguydood@outlook.com>
2025-07-31 21:20:50 -0500

    Create hilltopadstype1.js

---
+++ hilltopadstype1.js
@@ -0,0 +1,10 @@
+(function(xwy){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = xwy || {};
+s.src = "\/\/sneakyeffort.com\/bLXJV\/s.d\/GelQ0uYUWwcR\/he-mE9wuEZNUOlvkOP_T\/Y\/1vN\/THMHzDNbTKkEt\/NZjTUq1\/Miz\/My2IMaAi";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
