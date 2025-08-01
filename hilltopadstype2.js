3b10abb0fea9d091a69d87c7c37709979bbc86d1
KingNullboy <mjguydood@outlook.com>
2025-07-31 21:57:36 -0500

    Create hilltopadstype2.js

---
+++ hilltopadstype2.js
@@ -0,0 +1,10 @@
+(function(cjop){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = cjop || {};
+s.src = "\/\/smoggyfeed.com\/c.Do9J6\/b\/2c5jlYSKW-QN9eNTjwUp1MM\/z\/Ml4wM\/SD0P2WNXTRUlzJMezgg\/yQ";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
