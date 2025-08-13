45920ac4dec44c55fd4908bf0a46bfb23043dd42
KingNullboy <mjguydood@outlook.com>
2025-08-13 14:34:19 -0500

    css js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Ads script injection
     if (localStorage.getItem("ads") === "true" || localStorage.getItem("ads") === null) {
-        let adScript = document.createElement("script");
-        adScript.setAttribute("data-cfasync", "false");
-        adScript.setAttribute("type", "module");
-        adScript.setAttribute("async", "true");
-        adScript.src = "https://richinfo.co/richpartners/push/js/rp-cl-ob.js?pubid=969176&siteid=363261&niche=33";
-        document.head.appendChild(adScript);
-
-        let adScript2 = document.createElement("script");
-        adScript2.setAttribute("async", "true");
-        adScript2.src = "https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=969176&siteid=363262";
-        document.head.appendChild(adScript2);
-
         // Inject hilltopadstype1.js
         let hilltopScript1 = document.createElement("script");
         hilltopScript1.setAttribute("async", "true");
