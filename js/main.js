1ec0db74ae8813cc14f4db9ef3bfb14a9ebe4864
KingNullboy <mjguydood@outlook.com>
2025-09-07 12:37:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 async function checkPassword(input) {
     try {
-        const response = await fetch("https://nullwebsecurity.netlify.app/.netlify/functions/auth", {
+        const response = await fetch("https://nullapis.netlify.app/.netlify/functions/auth", {
             method: "GET",
             headers: {
                 "Script-URL": "https://www.null-web.vastserve.com/links/script.js",
