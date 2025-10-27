fe5b89ccb0e57c3cc6656cd46ece85533bb4c155
KingNullboy <mjguydood@outlook.com>
2025-10-27 08:06:53 -0500

    js

--- gxmes/search.js
+++ gxmes/search.js
@@ -4,10 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
   searchInput.addEventListener('input', () => {
     const filter = searchInput.value.toLowerCase();
     const buttons = document.querySelectorAll('button:not(header button)');
+    const headers = document.querySelectorAll('center h1, center h2');
 
     buttons.forEach(button => {
       const text = button.textContent.toLowerCase();
       button.style.display = text.includes(filter) ? 'inline-block' : 'none';
+    });
+    headers.array.forEach(element => {
+      element.style.display = searchInput.value === "" ? "block" : "none"
     });
   });
 });
