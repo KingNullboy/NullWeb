316834cfb667a45ef3e9427495a0afef253fa109
kingnullboy <kingnullboy@nullweb.org>
2026-06-18 23:56:18 -0500

    i started begging for money so I can buy an SSD

--- includes/footer.php
+++ includes/footer.php
@@ -19,7 +19,8 @@
 		<p>NullWeb is a web platform created by KingNullboy that brings together tools, games, wiki pages, and utilities in one place for fun, learning, and sharing.</p><br>
 		<p class="footer-note">
     		Due to unprecedented growth (three users online simultaneously), our infrastructure now requires immediate migration to next-generation NVMe technology.
-    		This investment will increase performance by approximately "a lot" and move us one step closer to becoming web scale or something.
+			This investment will increase performance by approximately "a lot" and move us one step closer to becoming web scale or something.
+			However, apparently AIs have been eating all the RAM and SSDs, and I am broke, so donations would be appreciated.
 		</p>
     </div>
 </footer>
