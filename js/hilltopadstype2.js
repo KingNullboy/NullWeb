cf348bd5dcc1bdeb6745981f1d151044e92cf6d9
kingnullboy <kingnullboy@proton.me>
2026-09-02 20:00:16 -0500

    updated hilltopads. finally nullweb.org is set up!

--- js/hilltopadstype2.js
+++ js/hilltopadstype2.js
@@ -3,7 +3,7 @@ var d = document,
 	s = d.createElement('script'),
 	l = d.scripts[d.scripts.length - 1];
 s.settings = cjop || {};
-s.src = "\/\/smoggyfeed.com\/c.Do9J6\/b\/2c5jlYSKW-QN9eNTjwUp1MM\/z\/Ml4wM\/SD0P2WNXTRUlzJMezgg\/yQ";
+s.src = "\/\/unfoldedtrade.com\/b\/X.V\/skdUG\/lm0bYjW\/cL\/Venml9huHZtUnllkEP\/TTcmzJNezxgo5fNMDpE-t\/NXzCMN3DOYDjkG0BNyQJ";
 s.async = true;
 s.referrerPolicy = 'no-referrer-when-downgrade';
 l.parentNode.insertBefore(s, l);
