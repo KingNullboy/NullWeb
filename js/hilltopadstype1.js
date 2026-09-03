3710374484b38143eb92fe3f7b5b68d1312054d5
kingnullboy <kingnullboy@nullweb.org>
2026-09-02 20:00:16 -0500

    updated hilltopads. finally nullweb.org is set up!

--- js/hilltopadstype1.js
+++ js/hilltopadstype1.js
@@ -3,7 +3,7 @@ var d = document,
 	s = d.createElement('script'),
 	l = d.scripts[d.scripts.length - 1];
 s.settings = xwy || {};
-s.src = "\/\/sneakyeffort.com\/bLXJV\/s.d\/GelQ0uYUWwcR\/he-mE9wuEZNUOlvkOP_T\/Y\/1vN\/THMHzDNbTKkEt\/NZjTUq1\/Miz\/My2IMaAi";
+s.src = "https://fluffy-machine.com/bG3.Vk0vP/3/p/vJbVmoV/JzZ/DQ0-3/MezYcX4YOOT-M/z_L/TlcIzSNkzWg/5nM/zgcG";
 s.async = true;
 s.referrerPolicy = 'no-referrer-when-downgrade';
 l.parentNode.insertBefore(s, l);
